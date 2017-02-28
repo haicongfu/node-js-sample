2af0f052fe79fb35c38a07f1952f8e3f828bdd29
EC2 Default User <ec2-user@ip-10-1-7-229.us-west-2.compute.internal>
2017-02-28 01:20:44 +0000

    first change

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express')
 var app = express()
+var logger = require('fluent-logger')
+
+logger.configure('fluentd.s3', {
+   host: 'localhost',
+   port: 24224,
+   timeout: 3.0,
+   reconnectInterval: 600000 // 10 minutes
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+  logger.emit('abcd', {record: 'test'});
   response.send('Hello World!')
 })
 
